6067eb30b3b3b6bb03ecba9491e019430afd07fa
GregTMJ <greg_1461994@hotmail.com>
2021-01-19 20:50:10 +0300

    CatTestCode: first code trial

---
+++ CatFunction.py
@@ -0,0 +1,15 @@
+class Cat:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def cat_name(self):
+        return self.name
+
+    def cat_age(self):
+        return self.age
+
+    def cat_gender(self):
+        return self.gender
+
---
+++ CatFunding.py
@@ -0,0 +1,7 @@
+from CatFunction import Cat
+
+Baron = Cat("Baron", 2, "Male")
+Sam = Cat("Sam", 2, "Male")
+print("the cats names are", Baron.name, "and", Sam.name)
+print("Cat is", Baron.age, "years old")
+print("The cat is a", Baron.gender)
